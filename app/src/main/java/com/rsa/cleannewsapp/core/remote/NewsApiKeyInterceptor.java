b63876c6c808ed0e590a0d2c1088cae892a2a44e
Reza Stallone <rezastallone@gmail.com>
2019-07-18 15:36:45 +0700

    Add news api remote service

---
+++ app/src/main/java/com/rsa/cleannewsapp/core/remote/NewsApiKeyInterceptor.java
@@ -0,0 +1,27 @@
+package com.rsa.cleannewsapp.core.remote;
+
+import com.rsa.cleannewsapp.BuildConfig;
+
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class NewsApiKeyInterceptor implements Interceptor {
+
+    private final static String NEWS_API_KEY = BuildConfig.NEWS_API_KEY;
+
+    private final static String NEWS_API_KEY_PARAM = "apiKey";
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        HttpUrl url = chain.request().url()
+            .newBuilder()
+            .addQueryParameter(NEWS_API_KEY_PARAM, NEWS_API_KEY)
+            .build();
+        Request request = chain.request().newBuilder().url(url).build();
+        return chain.proceed(request);
+    }
+}
---
+++ app/src/main/java/com/rsa/cleannewsapp/core/remote/RemoteServiceFactory.java
@@ -0,0 +1,34 @@
+package com.rsa.cleannewsapp.core.remote;
+
+import com.rsa.cleannewsapp.BuildConfig;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+
+public class RemoteServiceFactory {
+
+    private final static String BASE_URL =
+        BuildConfig.NEWS_API_DOMAIN + "/" + BuildConfig.NEWS_API_VERSION;
+
+    private RemoteServiceFactory() {
+
+    }
+
+    public static Retrofit getRemoteService() {
+        return new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(createClient())
+            .build();
+    }
+
+    private static OkHttpClient createClient() {
+        OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder();
+        addNewsApiKeyInterceptor(okHttpClientBuilder);
+        return okHttpClientBuilder.build();
+    }
+
+    private static void addNewsApiKeyInterceptor(OkHttpClient.Builder okHttpClientBuilder) {
+        okHttpClientBuilder.addInterceptor(new NewsApiKeyInterceptor());
+    }
+
+}
