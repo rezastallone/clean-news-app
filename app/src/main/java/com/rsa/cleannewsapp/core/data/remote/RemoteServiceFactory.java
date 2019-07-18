9987c8b39d156e901c4345e730dd0c1b3142ecb8
Reza Stallone <rezastallone@gmail.com>
2019-07-18 16:08:13 +0700

    Add retrofit rxjava adapter

--- app/src/main/java/com/rsa/cleannewsapp/core/data/remote/RemoteServiceFactory.java
+++ app/src/main/java/com/rsa/cleannewsapp/core/data/remote/RemoteServiceFactory.java
@@ -4,6 +4,7 @@ import com.rsa.cleannewsapp.BuildConfig;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 public class RemoteServiceFactory {
 
@@ -18,6 +19,7 @@ public class RemoteServiceFactory {
         return new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(createClient())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build();
     }
 
