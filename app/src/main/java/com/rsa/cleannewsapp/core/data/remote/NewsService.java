2bbb04d4089d7d882f511d3aa7e78971944c98df
Reza Stallone <rezastallone@gmail.com>
2019-07-18 17:20:27 +0700

    Add news api retrofit interface & service

---
+++ app/src/main/java/com/rsa/cleannewsapp/core/data/remote/NewsService.java
@@ -0,0 +1,20 @@
+package com.rsa.cleannewsapp.core.data.remote;
+
+import com.rsa.cleannewsapp.core.data.entity.NewsArticles;
+
+import io.reactivex.Observable;
+import retrofit2.Retrofit;
+
+public class NewsService implements NewsApi {
+
+    private final NewsApi newsApi;
+
+    public NewsService(Retrofit retrofit) {
+        newsApi = retrofit.create(NewsApi.class);
+    }
+
+    @Override
+    public Observable<NewsArticles> articles() {
+        return newsApi.articles();
+    }
+}
