ee9cf9fc8c22a4a3c9c6334acdc82a13af686564
Reza Stallone <rezastallone@gmail.com>
2019-07-18 22:31:29 +0700

    Add news article viewholder for headline list

---
+++ app/src/main/java/com/rsa/cleannewsapp/features/newsarticle/view/NewsArticleViewHolder.java
@@ -0,0 +1,39 @@
+package com.rsa.cleannewsapp.features.newsarticle.view;
+
+import com.rsa.cleannewsapp.R;
+import com.rsa.cleannewsapp.core.data.entity.Article;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class NewsArticleViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.iv_news_image)
+    ImageView IvNewsImage;
+
+    @BindView(R.id.tv_news_author)
+    TextView TvNewsAuthor;
+
+    @BindView(R.id.tv_news_description)
+    TextView TvNewsDescription;
+
+    @BindView(R.id.tv_news_title)
+    TextView TvNewsTitle;
+
+    public NewsArticleViewHolder(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(itemView);
+    }
+
+    public void bind(Article article){
+        TvNewsAuthor.setText(article.author);
+        TvNewsTitle.setText(article.title);
+        TvNewsDescription.setText(article.description);
+    }
+}
