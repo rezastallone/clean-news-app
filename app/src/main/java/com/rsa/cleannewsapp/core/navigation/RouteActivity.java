15e86e24e8f61dbbaafb443ff972dd1cda224167
Reza Stallone <rezastallone@gmail.com>
2019-07-18 14:27:41 +0700

    Add route activity as container for new fragment

---
+++ app/src/main/java/com/rsa/cleannewsapp/core/navigation/RouteActivity.java
@@ -0,0 +1,16 @@
+package com.rsa.cleannewsapp.core.navigation;
+
+import com.rsa.cleannewsapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+    }
+}
