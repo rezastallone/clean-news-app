13addd7f1e72ed3d7c3d544d01c1e866091f5ade
Reza Stallone <rezastallone@gmail.com>
2019-07-19 11:46:48 +0700

    Add news article remote data store

---
+++ data/src/main/java/com/rsa/cleannewsapp/core/data/repository/datasource/NewsArticleRemoteDataStore.java
@@ -0,0 +1,21 @@
+package com.rsa.cleannewsapp.core.data.repository.datasource;
+
+import com.rsa.cleannewsapp.core.data.remote.NewsApi;
+import com.rsa.cleannewsapp.core.domain.entity.NewsArticles;
+
+import io.reactivex.Observable;
+import retrofit2.Retrofit;
+
+public class NewsArticleRemoteDataStore implements NewsArticleStore {
+
+    private final NewsApi newsApi;
+
+    public NewsArticleRemoteDataStore(Retrofit retrofit) {
+        newsApi = retrofit.create(NewsApi.class);
+    }
+
+    @Override
+    public Observable<NewsArticles> headlines(String country) {
+        return newsApi.headlines(country);
+    }
+}
