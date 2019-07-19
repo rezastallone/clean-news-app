4cde106e4d27f3d5343ed64339ab2a1579d6aaa6
Reza Stallone <rezastallone@gmail.com>
2019-07-19 15:37:59 +0700

    Add get bookmarked news usecase in presenter

--- app/src/main/java/com/rsa/cleannewsapp/features/newsarticle/presenter/HeadlinePresenter.java
+++ app/src/main/java/com/rsa/cleannewsapp/features/newsarticle/presenter/HeadlinePresenter.java
@@ -1,6 +1,8 @@
 package com.rsa.cleannewsapp.features.newsarticle.presenter;
 
+import com.rsa.cleannewsapp.core.domain.entity.Article;
 import com.rsa.cleannewsapp.core.domain.entity.NewsArticles;
+import com.rsa.cleannewsapp.core.domain.usecase.GetBookmarkedNews;
 import com.rsa.cleannewsapp.core.domain.usecase.GetHeadline;
 import com.rsa.cleannewsapp.core.presenter.Presenter;
 import com.rsa.cleannewsapp.features.newsarticle.mapper.ArticleModelDataMapper;
@@ -16,6 +18,8 @@ import io.reactivex.observers.DisposableObserver;
 
 public class HeadlinePresenter extends Presenter<HeadlineView> {
 
+    private final GetBookmarkedNews getBookmarkedNews;
+
     private ArticleModelDataMapper articleModelDataMapper;
 
     private GetHeadline getHeadline;
@@ -23,16 +27,23 @@ public class HeadlinePresenter extends Presenter<HeadlineView> {
     @Inject
     public HeadlinePresenter(
         @NonNull GetHeadline getHeadline,
+        @NonNull GetBookmarkedNews getBookmarkedNews,
         ArticleModelDataMapper articleModelDataMapper
     ) {
         this.getHeadline = getHeadline;
         this.articleModelDataMapper = articleModelDataMapper;
+        this.getBookmarkedNews = getBookmarkedNews;
     }
 
     @Override
     public void initialize() {
         super.initialize();
         getView().showLoading();
+        executeGetBookmarkedNews();
+        executeGetHeadlines();
+    }
+
+    private void executeGetHeadlines() {
         getHeadline.execute(new DisposableObserver<NewsArticles>() {
             @Override
             public void onNext(NewsArticles newsArticles) {
@@ -52,6 +63,30 @@ public class HeadlinePresenter extends Presenter<HeadlineView> {
         });
     }
 
+    private void executeGetBookmarkedNews() {
+        getBookmarkedNews.execute(new DisposableObserver<ArrayList<Article>>() {
+            @Override
+            public void onNext(ArrayList<Article> articles) {
+                showBookmarkedNews(articles);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+    private void showBookmarkedNews(ArrayList<Article> bookmarkedNews) {
+        getView().showBookmarkedNews((ArrayList<ArticleModel>) articleModelDataMapper
+            .transform(bookmarkedNews));
+    }
+
     private void showHeadlines(NewsArticles newsArticles) {
         getView().showHeadlines(
             (ArrayList<ArticleModel>) articleModelDataMapper
@@ -60,6 +95,7 @@ public class HeadlinePresenter extends Presenter<HeadlineView> {
 
     public void destroy() {
         this.getHeadline.dispose();
+        this.getBookmarkedNews.dispose();
         setView(null);
     }
 }
