c9292019074d81106b4965768df8095cc3927f6a
Reza Stallone <rezastallone@gmail.com>
2019-07-19 15:20:05 +0700

    Add get bookmarked news and set bookmarked news to news repository

--- domain/src/main/java/com/rsa/cleannewsapp/core/domain/repository/NewsArticleRepository.java
+++ domain/src/main/java/com/rsa/cleannewsapp/core/domain/repository/NewsArticleRepository.java
@@ -1,11 +1,18 @@
 package com.rsa.cleannewsapp.core.domain.repository;
 
+import com.rsa.cleannewsapp.core.domain.entity.Article;
 import com.rsa.cleannewsapp.core.domain.entity.NewsArticles;
+
+import java.util.ArrayList;
 
 import io.reactivex.Observable;
 
 public interface NewsArticleRepository {
 
     Observable<NewsArticles> headlines(String country);
+
+    Observable<ArrayList<Article>> bookmarkedNews();
+
+    void setBookmarkedNews(ArrayList<Article> articles);
 
 }
