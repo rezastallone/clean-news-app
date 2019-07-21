c41b4dc301d7800e26f9ee03ba138838ab12396e
Reza Stallone <rezastallone@gmail.com>
2019-07-22 00:44:50 +0700

    Add isbookmarked field to article data

--- domain/src/main/java/com/rsa/cleannewsapp/core/domain/entity/Article.java
+++ domain/src/main/java/com/rsa/cleannewsapp/core/domain/entity/Article.java
@@ -1,7 +1,5 @@
 package com.rsa.cleannewsapp.core.domain.entity;
 
-import io.reactivex.annotations.NonNull;
-
 public class Article {
 
     public final String author;
@@ -9,6 +7,8 @@ public class Article {
     public final String content;
 
     public final String description;
+
+    public final Boolean isBookmarked;
 
     public final String publishedAt;
 
@@ -21,7 +21,7 @@ public class Article {
     public final String urlToImage;
 
     public Article(String sourceName, String author, String title, String description,
-        String url, String urlToImage, String publishedAt, String content) {
+        String url, String urlToImage, String publishedAt, String content, Boolean isBookmarked) {
         this.sourceName = sourceName;
         this.author = author;
         this.title = title;
@@ -30,5 +30,6 @@ public class Article {
         this.urlToImage = urlToImage;
         this.publishedAt = publishedAt;
         this.content = content;
+        this.isBookmarked = isBookmarked;
     }
 }
