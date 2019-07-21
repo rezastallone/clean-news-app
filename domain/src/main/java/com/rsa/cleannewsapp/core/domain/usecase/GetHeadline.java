fd7aa8a0948702f357b572ba7d1f1f2174af16a9
Reza Stallone <rezastallone@gmail.com>
2019-07-22 00:59:28 +0700

    Add filter headline news only get not bookmarked news

--- domain/src/main/java/com/rsa/cleannewsapp/core/domain/usecase/GetHeadline.java
+++ domain/src/main/java/com/rsa/cleannewsapp/core/domain/usecase/GetHeadline.java
@@ -3,6 +3,8 @@ package com.rsa.cleannewsapp.core.domain.usecase;
 import com.rsa.cleannewsapp.core.domain.entity.Article;
 import com.rsa.cleannewsapp.core.domain.repository.NewsArticleRepository;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -26,6 +28,30 @@ public class GetHeadline extends UseCase<List<Article>> {
 
     @Override
     protected Observable<List<Article>> createObservableUseCase() {
-        return this.newsArticleRepository.headlines("id");
+        return Observable.zip(this.newsArticleRepository.headlines("id"),
+            this.newsArticleRepository.bookmarkedNews(),
+            (articles, articles2) -> {
+
+                List<Article> filteredList = filterNotBookmarkedNewsOnly(articles, articles2);
+
+                return filteredList;
+            });
+    }
+
+    private List<Article> filterNotBookmarkedNewsOnly(List<Article> articles,
+        List<Article> articles2) {
+        List<Article> filteredList = new ArrayList<>();
+        HashSet<String> bookmarkedNews = new HashSet<>();
+
+        for (Article article : articles2) {
+            bookmarkedNews.add(article.url);
+        }
+
+        for (Article article : articles) {
+            if (!bookmarkedNews.contains(article.url)) {
+                filteredList.add(article);
+            }
+        }
+        return filteredList;
     }
 }
